736e5410bbf08c193c0592b0fc231064d598d73e
karvem <162024211+karvem@users.noreply.github.com>
2026-07-24 20:20:18 +0330

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.wifi_direct_chat"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
